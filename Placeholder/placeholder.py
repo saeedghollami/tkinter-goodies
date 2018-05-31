fe695a5ec2370ece4821a484abc7295a6ebda321
Saeed Gholami <saeed.ghollami@gmial.com>
2018-05-31 20:09:33 +0430

    determine which key remove palceholder and which one not

--- Placeholder/placeholder.py
+++ Placeholder/placeholder.py
@@ -13,8 +13,14 @@ from tkinter import ttk
 
 _flag = True  # if placeholder exsist this flag will be true otherwise False
 
+# the keys didn't remove placeholder when it exisit
+_no_keys = ("Tab", "Shift_L", "Control_L", "Control_L", "Return")
+
+
+def press_tab(event):
+	print(dir(event), event.keysym, event.keycode)
 # on_click event
-def onclick(event):
+def on_click(event):
 	global _flag
 
 	# if there is a placeholder cursor should be move postion 0
@@ -27,12 +33,22 @@ def onclick(event):
 def key_press(event):
 	global _flag
 
+	keysym = event.keysym
+	keycode = event.keycode
+
 	# if user type somthing when placeholder exsist 
-	if _flag:
+	# and user typed keycode is between 10 and 61
+	if _flag and 9 < keycode < 62 and keysym not in _no_keys:
 		event.widget.delete(0, END)  # delete placeholder
 		event.widget.configure(foreground='black')  # text should be black
 		_flag = False  # that's mean there isn't placeholder anymore.
-		
+
+	# if keysym is up down right left key
+	elif 110 < keycode < 115:
+		return 'break'    # do not call defualt event handler
+
+	print(keysym, keycode)
+	
 
 def on_remove(event, message=''):
 	global _flag
@@ -41,17 +57,15 @@ def on_remove(event, message=''):
 	# if there is a placeholder
 	if _flag:
 		event.widget.icursor(0)  # move the cursor to position 0
-		return 'break'  # don't remove any chars.
+		return 'break'  # don't run default event handler.
 
 	# display placeholder message if nothing was in the inside of widget.
-	elif not _flag and content_len < 1:
+	elif content_len < 1:
 		event.widget.delete(0, END)  # delete text of the widget
 		event.widget.insert(0, message)  # insert placeholder message
 		event.widget.icursor(0)  # move cursor to position 0
 		event.widget.configure(foreground= "gray")
 		_flag = True
-
-	print(_flag)
 
 
 # Add placeholder to the widget.
@@ -68,10 +82,9 @@ def placeholder(widget=None, message=''):
 	# creating custom event order
 	# PostRemove and PostClick will be evaluvate after default event("TEntry")
 	widget.bindtags(( str(widget), "TEntry", "PostRemove", "PostClick", ".", "all"))
-	widget.bind_class("PostClick", "<1>", onclick)
+	widget.bind_class("PostClick", "<1>", on_click)
 	widget.bind_class("PostRemove", "<BackSpace>", lambda e: on_remove(e, message))
-	# widget.bind_class("PostRemove", "<Delete>", lambda e: on_remove(e, message))
+	widget.bind_class("PostRemove", "<Delete>", lambda e: on_remove(e, message))
 	widget.bind("<Key>", key_press)
-	# widget.bind("<Tab>", on_remove)
 
 
