657d39ed698629292692e8a7fea4b3fff82a1767
Saeed Gholami <saeed.ghollami@gmial.com>
2018-06-01 09:07:42 +0430

    git ignore file for python

--- Placeholder/placeholder.py
+++ Placeholder/placeholder.py
@@ -6,7 +6,6 @@
 # 1test commend
 
 
-
 from tkinter import *
 from tkinter import ttk
 
@@ -14,22 +13,42 @@ from tkinter import ttk
 _flag = True  # if placeholder exsist this flag will be true otherwise False
 
 # the keys didn't remove placeholder when it exisit
-_no_keys = ("Tab", "Shift_L", "Control_L", "Control_L", "Return")
+# _no_keys = ("Tab", "Shift_L", "Control_L", "Control_L", "Return")
+_traked_keys = (
+	'1', '2', '3', '4', '5', '6', '7', '7', '8', '9', '0', 
+	'minus', 'equal', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 
+	'o', 'p', 'bracketleft', 'bracketright', 'backslash', 'a', 'grave',
+	's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'semicolon', 'apostrophe', 
+	'z', 'x', 'c', 'v', 'b', 'n', 'm', 'comma', 'period', 'slash', 'space', 
+	'asciitilde', 'exclam', 'at', 'at', 'numbersign', 'dollar', 'percent', 
+	'asciicircum', 'ampersand', 'asterisk', 'parenleft', 'parenright', 
+	'underscore', 'plus', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 
+	'P', 'braceleft', 'braceright', 'bar', 'A', 'S', 'D', 'F', 'G', 'H',
+	'J', 'K', 'L', 'colon', 'quotedbl', 'Z', 'X', 'C', 'V', 'B', 'N', 
+	'M', 'less', 'greater', 'question',  
+)
 
 
-def press_tab(event):
-	print(dir(event), event.keysym, event.keycode)
 # on_click event
+	# if there is a placeholder cursor should be move postion 0
 def on_click(event):
 	global _flag
 
-	# if there is a placeholder cursor should be move postion 0
 	if _flag:
 		event.widget.icursor(0)  # cursor move to postion 0
-	
-	# if there isn't any placeholder cursor will do it's normal job
-	
-# key press to on_remove placeholder message
+
+
+# double click event handler
+# if there is a placeholder double click should move cursor to pos 0
+def on_double_click(event):
+	global _flag
+
+	if _flag:
+		event.widget.icursor(0)
+		return 'break'
+
+
+# key press event handler
 def key_press(event):
 	global _flag
 
@@ -38,19 +57,15 @@ def key_press(event):
 
 	# if user type somthing when placeholder exsist 
 	# and user typed keycode is between 10 and 61
-	if _flag and 9 < keycode < 62 and keysym not in _no_keys:
+	if _flag and keysym in _traked_keys:
 		event.widget.delete(0, END)  # delete placeholder
 		event.widget.configure(foreground='black')  # text should be black
 		_flag = False  # that's mean there isn't placeholder anymore.
 
-	# if keysym is up down right left key
-	elif 110 < keycode < 115:
-		return 'break'    # do not call defualt event handler
 
-	print(keysym, keycode)
-	
+	print(keysym.__repr__(), keycode, _flag)
 
-def on_remove(event, message=''):
+def on_backspace(event, message=''):
 	global _flag
 	content_len = len(event.widget.get())
 
@@ -59,13 +74,31 @@ def on_remove(event, message=''):
 		event.widget.icursor(0)  # move the cursor to position 0
 		return 'break'  # don't run default event handler.
 
-	# display placeholder message if nothing was in the inside of widget.
+	# display placeholder message if nothing is inside of widget.
 	elif content_len < 1:
 		event.widget.delete(0, END)  # delete text of the widget
 		event.widget.insert(0, message)  # insert placeholder message
 		event.widget.icursor(0)  # move cursor to position 0
 		event.widget.configure(foreground= "gray")
 		_flag = True
+
+
+# Delete key event handler
+def on_delete(event, message):
+	global _flag
+	content_len = len(event.widget.get())
+
+	if _flag:
+		return 'break'
+
+	elif content_len < 1:
+		event.widget.delete(0, END)  # delete text of the widget
+		event.widget.insert(0, message)  # insert placeholder message
+		event.widget.icursor(0)  # move cursor to position 0
+		event.widget.configure(foreground= "gray")
+		_flag = True
+	print(_flag)
+	
 
 
 # Add placeholder to the widget.
@@ -83,8 +116,9 @@ def placeholder(widget=None, message=''):
 	# PostRemove and PostClick will be evaluvate after default event("TEntry")
 	widget.bindtags(( str(widget), "TEntry", "PostRemove", "PostClick", ".", "all"))
 	widget.bind_class("PostClick", "<1>", on_click)
-	widget.bind_class("PostRemove", "<BackSpace>", lambda e: on_remove(e, message))
-	widget.bind_class("PostRemove", "<Delete>", lambda e: on_remove(e, message))
+	widget.bind_class("PostRemove", "<BackSpace>", lambda e: on_backspace(e, message))
+	widget.bind_class("PostRemove", "<Delete>", lambda e: on_backspace(e, message))
+	widget.bind("<Double-Button-1>", on_double_click)
 	widget.bind("<Key>", key_press)
 
 
